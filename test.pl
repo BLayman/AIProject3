f00856bda8ab3d4320cd895553d42ea6f6a35330
LaymanB <leasure57@gmail.com>
2018-11-28 17:02:56 -0700

    Implemented a bit of logic to decide if a wumpus is on a particular grid spot. The check is not complete, as it only checks one possibility, but shows that knowledge can be added via python.

--- test.pl
+++ test.pl
@@ -8,3 +8,16 @@ avoid(X) :-
 dangerous(X),
 \+ dead(X),
 notify(X).
+
+
+stench(0,1).
+stench(1,0).
+
+% note: this is not complete!
+hasWumpus(X,Y) :-
+  A is X - 1,
+  B is Y - 1,
+  stench(A ,Y),
+  stench(X, B),
+  wumpusOn(X,Y).
+
--- test.py
+++ test.py
@@ -7,14 +7,28 @@ from pyswip.easy import registerForeign
 def notify(x):
     print("%s should be avoided" % x)
 
+def killWumpus(prolog):
+    prolog.assertz("dead(wumpus)")
+
+def wumpusOn(x, y):
+    print("hello")
+    print("wumpus is on %d %d " %(x,y))
+
 
 def main():
-    notify.arity = 1
     prolog = Prolog()
+    notify.arity = 1
     registerForeign(notify)
+    wumpusOn.arity = 2
+    registerForeign(wumpusOn)
+
     prolog.consult("test.pl")
-    list(prolog.query("avoid(wumpus)"))
-    list(prolog.query("avoid(dinosaurs)"))
+    prolog.assertz("stench(1,2)")
+    prolog.assertz("stench(2,1)")
+
+    list(prolog.query("hasWumpus(2,2)"))
+    #list(prolog.query("avoid(wumpus)"))
+    #list(prolog.query("avoid(dinosaurs)"))
 
 
 if __name__ == "__main__":
