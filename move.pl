29f5208d891b2677807d03dfb91a25cfa2bb6b53
LaymanB <leasure57@gmail.com>
2018-11-30 18:41:33 -0700

    added code for picking up glitter and adding to player points.

--- move.pl
+++ move.pl
@@ -6,16 +6,27 @@
 
 :- dynamic hasStench/2.
 :- dynamic hasBreeze/2.
+:- dynamic hasGlitter/2.
 
 :- dynamic confirmStench/2.
 :- dynamic confirmBreeze/2.
+:- dynamic confirmGlitter/2.
 
+:- dynamic playerPoints/1.
+
+playerPoints(0).
+
+% test conditions
 hasStench(1,1).
 hasBreeze(1,1).
+hasGlitter(2,2).
 
+% confirm percepts when entering a space
 move(X,Y) :-
   confirmIfBreeze(X,Y);
-  confirmIfStench(X,Y).
+  confirmIfStench(X,Y);
+  confirmIfGlitter(X,Y);
+  true().
 
 confirmIfBreeze(X,Y) :-
   hasBreeze(X,Y),
@@ -24,6 +35,22 @@ confirmIfBreeze(X,Y) :-
 
 confirmIfStench(X,Y) :-
   hasStench(X,Y),
-  assert(confirmStench(X,Y)).
+  assert(confirmStench(X,Y)),
+  fail().
 
-  isHappy(panda).
+confirmIfGlitter(X,Y) :-
+  hasGlitter(X,Y),
+  assert(confirmGlitter(X,Y)),
+  pickUpGlitter(),
+  fail().
+
+
+% if glitter is percieved, pick it up, and increase player points
+pickUpGlitter() :-
+  increaseCreaturePoints(1000).
+
+increaseCreaturePoints(X) :-
+  playerPoints(Z),
+  Y is X + Z,
+  retract(playerPoints(Z)),
+  assert(playerPoints(Y)).
