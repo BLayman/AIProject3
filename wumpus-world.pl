838fc80a76860272e5aeec18488671bfa97dffad
jordanpottruff <jordanpottruff@gmail.com>
2018-12-02 12:02:47 -0700

    Added my wumpus-world

---
+++ wumpus-world.pl
@@ -0,0 +1,121 @@
+
+:- dynamic foundBreeze/2.
+:- dynamic foundStench/2.
+:- dynamic foundGlitter/2.
+:- dynamic visited/2.
+:- dynamic bump/2.
+
+width(4).
+height(4).
+
+% hidden information
+
+hasWumpus(1,3).
+hasPit(3,1).
+hasPit(3,3).
+hasPit(4,4).
+hasGold(2,3).
+
+cell(1,1).
+
+neighbor(X1, Y1, X2, Y2) :-
+  cell(X1, Y1),
+  cell(X2, Y2),
+  (above(X1, Y1, X2, Y2);
+  below(X1, Y1, X2, Y2);
+  right(X1, Y1, X2, Y2);
+  left(X1, Y1, X2, Y2)).
+
+
+above(X1, Y1, X2, Y2) :-
+  X2 =:= X1,
+  Y2 =:= Y1+1.
+
+below(X1, Y1, X2, Y2) :-
+  X2 =:= X1,
+  Y2 =:= Y1-1.
+
+right(X1, Y1, X2, Y2) :-
+  Y2 =:= Y1,
+  X2 =:= X1+1.
+
+left(X1, Y1, X2, Y2) :-
+  Y2 =:= Y1,
+  X2 =:= X1-1.
+
+% predicates for determining percept assignments.
+
+hasStench(X, Y) :-
+  neighbor(X, Y, NX, NY),
+  hasWumpus(NX, NY).
+
+hasBreeze(X, Y) :-
+  neighbor(X, Y, NX, NY),
+  hasPit(NX, NY).
+
+hasGlitter(X, Y) :-
+  hasGold(X, Y).
+
+% predicates for deducing whether entities exist given perception.
+
+noPit(X, Y) :-
+  neighbor(X, Y, NX, NY),
+  visited(NX, NY),
+  not(foundBreeze(NX, NY)).
+
+noWumpus(X, Y) :-
+  neighbor(X, Y, NX, NY),
+  visited(NX, NY),
+  not(foundStench(NX, NY)).
+
+neighborVisited(X, Y) :-
+  neighbor(X, Y, NX, NY),
+  visited(NX, NY).
+
+isSafe(X, Y) :-
+  visited(X,Y);
+  (noWumpus(X, Y),
+  noPit(X, Y)).
+
+isUnvisitedSafe(X, Y) :-
+  cell(X,Y),
+  not(visited(X,Y)),
+  isSafe(X,Y).
+
+% for discovering new percepts at a visited cell.
+
+inBounds(X, Y) :-
+  cell(X,Y),
+  width(W),
+  height(H),
+  X >= 1,
+  X =< W,
+  Y >= 1,
+  Y =< H.
+
+visit(X, Y) :-
+  (visited(X,Y) -> false; true),
+  assertz(visited(X,Y)),
+  (inBounds(X,Y) -> true ; assertz(bump(X,Y)), false),
+  XP is X+1,
+  XM is X-1,
+  YP is Y+1,
+  YM is Y-1,
+  assertz(cell(X,YM)),
+  assertz(cell(X,YP)),
+  assertz(cell(XM,Y)),
+  assertz(cell(XP,Y)),
+  gatherPercepts(X, Y).
+
+gatherPercepts(X, Y) :-
+  (hasBreeze(X, Y) -> assertz(foundBreeze(X, Y)); true),
+  (hasStench(X, Y) -> assertz(foundStench(X, Y)); true),
+  (hasGlitter(X, Y) -> assertz(foundGlitter(X, Y)); true).
+
+foundWumpus(X,Y) :-
+  neighbor(X,Y,AX,AY),
+  neighbor(X,Y,BX,BY),
+  not((AX == BX, AY == BY)),
+  hasStench(AX, AY),
+  hasStench(BX, BY),
+  not(isSafe(X,Y)).
---
+++ wumpus-world.py
@@ -0,0 +1,166 @@
+from pyswip.prolog import Prolog
+from pyswip.easy import registerForeign
+import random
+import math
+
+
+def startGame(prolog):
+
+    points = 0
+
+    position = (1,1)
+    list(prolog.query("visit(%s,%s)" % position))
+    direction = 1 # up=0, right=1, down=2, left=3
+
+
+    path = []
+
+    i = 0
+    while(i<20):
+
+        unvisitedCellsDict = list(prolog.query("isUnvisitedSafe(X,Y)"))
+        unvisitedCells = toTupleList(unvisitedCellsDict)
+
+        visitedCellsDict = list(prolog.query("visited(X,Y)"))
+        visitedCells = toTupleList(visitedCellsDict)
+
+        print("Position: " + str(position))
+        currentGoal = closestUnvisited(position, unvisitedCells)
+        print("Goal: " + str(currentGoal))
+        projectedPath = shortestPath(position, currentGoal, visitedCells)
+
+        print("Projected path: " + str(projectedPath))
+
+        print("-----------")
+
+        previousPosition = position
+        for cell in projectedPath:
+            needToFace = directionToFaceNext(position, cell)
+
+            # cost of turning...
+            difference = abs(needToFace-direction)
+            if difference == 3:
+                points += -1
+            else:
+                points += -difference
+
+            direction = needToFace
+
+            # cost of moving...
+            points += -1
+
+            # update position
+            previousPosition = position
+            position = cell
+
+        list(prolog.query("visit(%s,%s)" % position))
+        # revert to 2nd to last position if we bump
+        bump = len(list(prolog.query("bump(%s,%s)" % (position[0],position[1])))) > 0
+
+        # if bumped, just move back. Cost of bump already taken into account.
+        if bump:
+            position = previousPosition
+
+        print(position)
+
+        i += 1
+
+    print(list(prolog.query("visited(X,Y)")))
+
+def directionToFaceNext(origin, next):
+    if(origin[0] == next[0]):
+        if(origin[1] < next[1]):
+            return 0
+        else:
+            return 2
+    else:
+        if(origin[0] < next[0]):
+            return 1
+        else:
+            return 3
+
+def closestUnvisited(start, safeUnvisited):
+    minDist = math.inf
+    min = (None, None)
+
+    for cell in safeUnvisited:
+        dist = abs(start[0]-cell[0]) + abs(start[1]-cell[1])
+
+        if dist < minDist:
+            minDist = dist
+            min = cell
+
+    return min
+
+def shortestPath(start, end, visited):
+    marked = [start]
+    frontier = [start]
+    parents = {}
+
+    while len(frontier) != 0:
+        cell = frontier[0]
+        del frontier[0]
+
+
+        if isNeighbor(end, cell):
+            path = [end]
+            iterator = cell
+            while(iterator != start):
+                iterator = parents[iterator]
+                path.append(iterator)
+            path.reverse()
+            return path
+
+
+        for neighbor in neighbors(cell, visited):
+            if neighbor in marked:
+                continue
+            print("Neighbor" + str(neighbor))
+            marked.append(neighbor)
+            frontier.append(neighbor)
+            parents[neighbor] = cell
+
+
+def neighbors(cell, cellList):
+    neighbors = []
+    for c in cellList:
+        if isNeighbor(cell, c):
+            neighbors.append(c)
+    return neighbors
+
+def isNeighbor(cellA, cellB):
+    vertical = cellA[0] == cellB[0] and abs(cellA[1]-cellB[1]) == 1
+    horizontal = cellA[1] == cellB[1] and abs(cellA[0]-cellB[0]) == 1
+    return vertical or horizontal
+
+def toTupleList(cellDict):
+    tlist = []
+    for cell in cellDict:
+        x = cell['X']
+        y = cell['Y']
+        tlist.append((x,y))
+    return list(set(tlist))
+
+
+
+def nextMove(position, safeCells):
+    adjacentCells = []
+    for cell in safeCells:
+        vertical = cell[0] == position[0] and abs(cell[1]-position[1]) == 1
+        horizontal = cell[1] == position[1] and abs(cell[0]-position[0]) == 1
+        if vertical or horizontal:
+            adjacentCells.append(cell)
+
+    nextCell = random.choice(adjacentCells)
+    return nextCell
+
+
+
+
+if __name__ == '__main__':
+    prolog = Prolog()
+    prolog.consult("wumpus-world.pl")
+
+    startGame(prolog)
+
+    #print(shortestPath((1,1), (4,4), [(1,1),(2,1),(3,1),(4,1),(4,2),(2,2),(2,3),(2,4),(3,4),(4,4),(4,3)]))
